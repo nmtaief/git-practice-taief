43c7e3e6a5f09ab999b6de00187e7accbe59c101
Noor Mohammed Taief <your-email@example.com>
2026-06-13 23:08:38 +0600

    Initial commit: project structure and basic program

---
+++ README.md
@@ -0,0 +1,5 @@
+Project name : Git Practice Project
+
+Author: Noor Mohammed Taief
+
+This project is a basic program to demonstrate Git and GitHub workflow.
---
+++ src/main.py
@@ -0,0 +1,8 @@
+import datetime
+
+def main():
+    print("Name: Noor Mohammed Taief")
+    print(f"Today's date: {datetime.date.today()}")
+
+if __name__ == "__main__":
+    main()
